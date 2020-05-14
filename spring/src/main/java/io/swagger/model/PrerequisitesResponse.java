1be10652afb9f0b49cc2665f5a7d6add4dff914d
SwaggerHub <SwaggerHub@smartbear.com>
2020-05-14 18:52:34 +0000

    API definition transferred by SwaggerHub

---
+++ spring/src/main/java/io/swagger/model/PrerequisitesResponse.java
@@ -0,0 +1,78 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * PrerequisitesResponse
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-05-14T18:52:33.737Z[GMT]")
+public class PrerequisitesResponse   {
+  @JsonProperty("token")
+  private String token = null;
+
+  public PrerequisitesResponse token(String token) {
+    this.token = token;
+    return this;
+  }
+
+  /**
+   * Get token
+   * @return token
+  **/
+  @ApiModelProperty(example = "123456789", value = "")
+  
+    public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PrerequisitesResponse prerequisitesResponse = (PrerequisitesResponse) o;
+    return Objects.equals(this.token, prerequisitesResponse.token);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(token);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PrerequisitesResponse {\n");
+    
+    sb.append("    token: ").append(toIndentedString(token)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
